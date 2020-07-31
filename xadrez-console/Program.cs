7000c9f4c5bf30c4cb0a267d3227217e5b60aba4
Bruno Sabia <brunosabia1@gmail.com>
2020-07-31 17:08:20 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,14 +10,8 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
-            Console.ReadLine();
-
-            /*
+            
             try
             { 
                 Tabuleiro tab = new Tabuleiro(8, 8);
@@ -28,13 +22,15 @@ namespace xadrez_console
 
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
-            */
+            
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -12,16 +12,44 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
+
                 for (int j = 0; j < tab.colunas; j++)
                 {
+                    
+
                     if(tab.peca(i,j) == null)
                     {
                         Console.Write("- ");
                     }
-
-                    Console.Write( tab.peca(i,j) + " ");
+                    else
+                    {
+                        imprimirPeca(tab.peca(i, j));
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        //Método para controlar as cores das pecas
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                //Linha abaixo salva a cor atual pra "aux"
+                ConsoleColor aux = Console.ForegroundColor;
+                //Muda a cor padrão para yellow
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                //imprime a peca na cor padrão atual (yellow)
+                Console.Write(peca);
+                //volta a cor padrão original
+                Console.ForegroundColor = aux;
             }
         }
     }
