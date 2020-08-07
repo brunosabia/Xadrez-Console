9760f380a2e37e89b6609d6517294ed29b2b7a9e
Bruno Sabia <brunosabia1@gmail.com>
2020-08-07 17:10:54 -0300

    Acrescentando as outras pecas

---
+++ xadrez-console/xadrez/Cavalo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        //método para verificar se pode mover a peca
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            //criar uma matriz do mesmo tamanho que o tabuleiro
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 2);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            pos.definirValores(posicao.linha - 2, posicao.coluna -1);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            pos.definirValores(posicao.linha - 2, posicao.coluna + 1);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 2);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 2);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+            pos.definirValores(posicao.linha + 2, posicao.coluna + 1);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+
+
+            pos.definirValores(posicao.linha + 2 , posicao.coluna - 1);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            //selecionar a posição NOROESTE da peça
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 2);
+            //SE a posição for valida(dentro do tabuleiro e maior que 0) 
+            //Se a posicao destino estiver vazia ou for da cor do time oposto, a posição é valida.
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "C ";
+        }
+    }
+
+}
---
+++ xadrez-console/xadrez/Dama.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Dama : Peca
+    {
+        public Dama(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override string ToString()
+        {
+            return "D ";
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            //criar uma matriz do mesmo tamanho que o tabuleiro
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //selecionar a posição ACIMA da peça
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.linha -= 1;
+
+            }
+
+
+
+
+            //selecionar a posição ABAIXO da peça
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.linha += 1;
+
+            }
+
+            //selecionar a posição DIREITA
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.coluna += 1;
+
+            }
+
+            //selecionar a posição ESQUERDA
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.coluna -= 1;
+
+            }
+
+
+            //diagonais:
+
+            //NOROESTE
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.definirValores(pos.linha - 1, pos.coluna - 1);
+
+            }
+
+            
+
+
+            //selecionar a posição NORDESTE da peça
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.definirValores(pos.linha - 1, pos.coluna + 1);
+
+            }
+
+            //selecionar a posição SUDOESTE
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.definirValores(pos.linha + 1, pos.coluna - 1);
+
+            }
+
+            //selecionar a posição SUDESTE
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            //enquanto a posição estiver vazia e for valida, marcar a posição como livre(true)
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+
+                // caso a posição nao seja viavel, parar de tornar as posicoes true
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                //caso a posição seja viavel, ela vai passar pelo if acima e vai subir para a linha de cima (linha - 1)
+                pos.definirValores(pos.linha + 1, pos.coluna + 1);
+
+            }
+
+
+                return mat;
+        }
+    }
+
+}
